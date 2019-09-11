13ffd3ed655f3916ff546d6c70b0c817698b48c9
Adriel <adriel.idf@gmail.com>
2019-09-11 20:13:22 -0300

    started game world terraiin generation script

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Editor Variables
+    [SerializeField]
+    private GameObject GrassPrefab = null;
+    [SerializeField]
+    private GameObject DirtPrefab = null;
+    [SerializeField]
+    private GameObject StonePrefab = null;
+    [SerializeField]
+    private int ChunkSize = 32;
+    [SerializeField]
+    private int Smoothness = 15;
+    [SerializeField]
+    private float MapSeed = 16;
+    #endregion
+
+
     void Start()
     {
-        
+        GenerateChunk();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void GenerateChunk()
     {
-        
+        int chunkHeight = 0;
+        for (int i = 0; i < ChunkSize; i++)
+        {
+            int h = Mathf.RoundToInt(Mathf.PerlinNoise(MapSeed, i / Smoothness) * ChunkSize) + chunkHeight;
+            for (int j = 0; j < h; j++)
+            {
+                Instantiate(GetHeightPrefab(j), new Vector3(i, j), Quaternion.identity);
+            }
+        }
+
+    }
+    private GameObject GetHeightPrefab(int height)
+    {
+        return DirtPrefab;
     }
 }
